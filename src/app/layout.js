9d47e95ea0ddf61160d7ed96cb475bcf77c9813f
dev7 <dev7.ngenit@gmail.com>
2025-05-18 12:59:32 +0600

    add: created the base configuration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Hanken_Grotesk, Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const grotesk = Hanken_Grotesk({
+  variable: "--font-hanken-grotesk",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
@@ -19,9 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${poppins.variable} ${grotesk.variable} antialiased`}>
         {children}
       </body>
     </html>
