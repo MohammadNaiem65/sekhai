3d73039ddb048f080f2a0204912d9ee3839b5341
dev7 <dev7.ngenit@gmail.com>
2025-05-20 16:55:30 +0600

    add: created OnDemandSkillsPromo component

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/home/hero";
 import MasonrySection from "@/components/home/masonrySection/masonrySection";
+import OnDemandSkillsPromo from "@/components/home/OnDemandSkillsPromo";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <MasonrySection />
+      <OnDemandSkillsPromo />
     </>
   );
 }
---
+++ src/components/home/OnDemandSkillsPromo.jsx
@@ -0,0 +1,14 @@
+export default function OnDemandSkillsPromo() {
+  return (
+    <section className="mx-auto mt-[6.25rem]">
+      <h1 className="text-center text-hero-headline font-semibold text-title-one">
+        Start Learning Today <br /> 500+ Learning Module Available
+      </h1>
+      <hr className="mx-auto mt-7 mb-6 h-[3px] w-[12.875rem] bg-stroke" />
+      <p className="mx-auto w-[47.375rem] text-center text-xl text-gray">
+        Gain in-demand skills and knowledge through interactive and engaging
+        online learning, and take the next step toward a successful future.
+      </p>
+    </section>
+  );
+}
